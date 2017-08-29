3edc183427dd74caafb3c4aafc6a203d10d0df54
sw-yx <swyx@swyx.io>
2017-08-29 16:41:26 -0400

    commit to heroku

--- db/index.js
+++ db/index.js
@@ -4,8 +4,8 @@ const chalk = require('chalk');
 const Sequelize = require('sequelize');
 const pkg = require('../package.json');
 
-const name = process.env.DATABASE_URL || (process.env.DATABASE_NAME || pkg.name)
-const connectionString = process.env.DATABASE_connectionString || `postgres://localhost:5432/${pkg.name}`;
+const name = process.env.DATABASE_NAME || pkg.name
+const connectionString = process.env.DATABASE_URL || (process.env.DATABASE_connectionString || `postgres://localhost:5432/${pkg.name}`)
 
 console.log(chalk.yellow(`Opening database connection to ${connectionString}`));
 
